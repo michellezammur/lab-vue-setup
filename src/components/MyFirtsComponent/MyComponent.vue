845da0e5649ada28324b8ef573c6aa43d90107d0
Michelle Zammur <mzammur@gmail.com>
2022-09-27 11:21:51 +0200

    first commit

---
+++ src/components/MyFirtsComponent/MyComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Hello World</h1>
+    <button @click="helloWorld"> CLICK ME! </button>
+
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+
+ 
+function helloWorld() {
+    console.log("Hello World");
+    alert("hello world");
+}
+
+helloWorld();
+  
+  </script>
+  
+  
+  <style scoped>
+
+    button {
+      padding: 2rem;
+      border-radius: 10%;
+      margin-left: 2rem;
+    }
+
+   
+  </style>
